bfe3ebd9f5026b7228ab4a450726d08dde7b4ef6
daurnimator <quae@daurnimator.com>
2017-05-17 17:34:37 +1000

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const BabiliPlugin = require("babili-webpack-plugin");
+
+module.exports = [
+	{
+		entry: './src/jslib.js',
+		target: 'web',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'fengari_interop.js',
+			library: 'fengari_interop'
+		},
+		externals: {
+			"util": "util",
+			"fengari": "fengari"
+		},
+		plugins: [
+		]
+	},
+	{
+		entry: './src/jslib.js',
+		target: 'web',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'fengari_interop.min.js',
+			library: 'fengari_interop'
+		},
+		externals: {
+			"util": "util",
+			"fengari": "fengari"
+		},
+		plugins: [
+			new BabiliPlugin()
+		]
+	}
+];
