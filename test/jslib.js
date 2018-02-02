39935e031f9c10e924980461e3fcf877cf906e18
daurnimator <quae@daurnimator.com>
2018-02-02 12:36:00 -0800

    test/jslib.js: Destructure to function level

--- test/jslib.js
+++ test/jslib.js
@@ -1,63 +1,74 @@
 "use strict";
 
 const {
-	lua,
-	lauxlib,
-	lualib,
+	lua: {
+		LUA_OK,
+		lua_pcall,
+		lua_tojsstring
+	},
+	lauxlib: {
+		luaL_dostring,
+		luaL_loadstring,
+		luaL_newstate,
+		luaL_requiref
+	},
+	lualib: {
+		luaL_openlibs
+	},
 	to_luastring
 } = require("fengari");
 const assert = require("assert");
 
 describe("fengari-interop", function() {
 	it("loads successfully", function() {
-		const jslib = require("../src/jslib.js");
-		assert(typeof jslib.luaopen_js === "function");
+		const { luaopen_js } = require("../src/jslib.js");
+		assert(typeof luaopen_js === "function");
 	});
 
 	const new_state = function() {
-		const jslib = require("../src/jslib.js");
+		const { luaopen_js } = require("../src/jslib.js");
 
-		const L = lauxlib.luaL_newstate();
-		lualib.luaL_openlibs(L);
-		lauxlib.luaL_requiref(L, to_luastring("js"), jslib.luaopen_js, 0);
+		const L = luaL_newstate();
+		luaL_openlibs(L);
+		luaL_requiref(L, to_luastring("js"), luaopen_js, 0);
 		return L;
 	};
 
 	it("can be required from lua", function() {
 		const L = new_state();
-		if (lauxlib.luaL_dostring(L, to_luastring('require("js")')) !== lua.LUA_OK) {
-			throw lua.lua_tojsstring(L, -1);
+		if (luaL_dostring(L, to_luastring('require("js")')) !== LUA_OK) {
+			throw lua_tojsstring(L, -1);
 		}
 	});
 
 	it("pushes same null every time", function() {
-		const jslib = require("../src/jslib.js");
+		const { push, tojs } = require("../src/jslib.js");
 		const L = new_state();
-		if (lauxlib.luaL_loadstring(L, to_luastring(`
+		if (luaL_loadstring(L, to_luastring(`
 		local null = ...
 		local js = require "js"
 		assert(null == js.null)
 		assert(rawequal(null, js.null))
-		`)) !== lua.LUA_OK) {
-			throw lua.lua_tojsstring(L, -1);
+		`)) !== LUA_OK) {
+			throw lua_tojsstring(L, -1);
 		}
-		jslib.push(L, null);
-		if (lua.lua_pcall(L, 1, 0, 0) !== lua.LUA_OK) {
-			throw jslib.tojs(L, -1);
+		push(L, null);
+		if (lua_pcall(L, 1, 0, 0) !== LUA_OK) {
+			throw tojs(L, -1);
 		}
 	});
 
 	it("allows calls with no 'this' or arguments", function() {
-		const jslib = require("../src/jslib.js");
+		const { tojs } = require("../src/jslib.js");
 		const L = new_state();
-		if (lauxlib.luaL_loadstring(L, to_luastring(`
+		if (luaL_loadstring(L, to_luastring(`
 		local js = require "js"
 		js.global.Date.now()
-		`)) !== lua.LUA_OK) {
-			throw lua.lua_tojsstring(L, -1);
+		`)) !== LUA_OK) {
+			throw lua_tojsstring(L, -1);
 		}
-		if (lua.lua_pcall(L, 0, 0, 0) !== lua.LUA_OK) {
-			throw jslib.tojs(L, -1);
+		if (lua_pcall(L, 0, 0, 0) !== LUA_OK) {
+			throw tojs(L, -1);
 		}
 	});
 });
